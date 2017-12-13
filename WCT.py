671f9a1cdf0ca7de7c6e93417224892987635279
sunshineatnoon <8500575@qq.com>
2017-12-13 15:08:28 -0800

    faster

--- WCT.py
+++ WCT.py
@@ -4,7 +4,6 @@ import argparse
 from PIL import Image
 from torch.autograd import Variable
 import torchvision.utils as vutils
-import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 from Loader import Dataset
 from util import *
@@ -43,104 +42,45 @@ dataset = Dataset(args.contentPath,args.stylePath,args.fineSize)
 loader = torch.utils.data.DataLoader(dataset=dataset,
                                      batch_size=1,
                                      shuffle=False)
-def scale_dialate(seg,W,H):
-    # TODO: dialate
-    seg = seg.unsqueeze(0)
-    seg = seg.expand(3,seg.size(1),seg.size(2))
-    seg = transforms.ToPILImage()(seg)
-    seg = seg.resize((H,W))
-    seg = transforms.ToTensor()(seg)[0].squeeze(0)
-    return seg
 
-def wct2(cF,sF):
-    cFSize = cF.size()
-    c_mean = torch.mean(cF,1) # c x (h x w)
-    c_mean = c_mean.unsqueeze(1).expand_as(cF)
-    cF = cF - c_mean
-
-    contentConv = torch.mm(cF,cF.t()).div(cFSize[1]-1) + torch.eye(cFSize[0]).double()
-    c_u,c_e,c_v = torch.svd(contentConv,some=False)
-
-    k_c = cFSize[0]
-    for i in range(cFSize[0]):
-        if c_e[i] < 0.00001:
-            k_c = i
-            break
-
-    sFSize = sF.size()
-    s_mean = torch.mean(sF,1)
-    sF = sF - s_mean.unsqueeze(1).expand_as(sF)
-    styleConv = torch.mm(sF,sF.t()).div(sFSize[1]-1)
-    s_u,s_e,s_v = torch.svd(styleConv,some=False)
-
-    k_s = sFSize[0]
-    for i in range(sFSize[0]):
-        if s_e[i] < 0.00001:
-            k_s = i
-            break
-
-    c_d = (c_e[0:k_c]).pow(-0.5)
-    step1 = torch.mm(c_v[:,0:k_c],torch.diag(c_d))
-    step2 = torch.mm(step1,(c_v[:,0:k_c].t()))
-    whiten_cF = torch.mm(step2,cF)
-
-    s_d = (s_e[0:k_s]).pow(0.5)
-    targetFeature = torch.mm(torch.mm(torch.mm(s_v[:,0:k_s],torch.diag(s_d)),(s_v[:,0:k_s].t())),whiten_cF)
-    targetFeature = targetFeature + s_mean.unsqueeze(1).expand_as(targetFeature)
-    return targetFeature
-
-def feature_wct(cF,sF,csF):
-    cF = cF.double()
-    sF = sF.double()
-    C,W,H = cF.size(0),cF.size(1),cF.size(2)
-    _,W1,H1 = sF.size(0),sF.size(1),sF.size(2)
-    cFView = cF.view(C,-1)
-    sFView = sF.view(C,-1)
-
-    targetFeature = wct2(cFView,sFView)
-    targetFeature = targetFeature.view_as(cF)
-    ccsF = args.alpha * targetFeature + (1.0 - args.alpha) * cF
-    ccsF = ccsF.float().unsqueeze(0)
-    csF.data.resize_(ccsF.size()).copy_(ccsF)
-    return csF
-
+wct = WCT(args)
+wct.cuda(0)
 def styleTransfer(contentImg,styleImg,imname,csF):
-    e1,d1,e2,d2,e3,d3,e4,d4,e5,d5 = loadModel(args)
 
-    sF5 = e5(styleImg)
-    cF5 = e5(contentImg)
+    sF5 = wct.e5(styleImg)
+    cF5 = wct.e5(contentImg)
     sF5 = sF5.data.cpu().squeeze(0)
     cF5 = cF5.data.cpu().squeeze(0)
-    csF5 = feature_wct(cF5,sF5,csF)
-    Im5 = d5(csF5)
+    csF5 = wct.transform(cF5,sF5,csF,args.alpha)
+    Im5 = wct.d5(csF5)
 
-    sF4 = e4(Im5)
-    cF4 = e4(contentImg)
+    sF4 = wct.e4(Im5)
+    cF4 = wct.e4(contentImg)
     sF4 = sF4.data.cpu().squeeze(0)
     cF4 = cF4.data.cpu().squeeze(0)
-    csF4 = feature_wct(cF4,sF4,csF)
-    Im4 = d4(csF4)
+    csF4 = wct.transform(cF4,sF4,csF,args.alpha)
+    Im4 = wct.d4(csF4)
 
-    sF3 = e3(styleImg)
-    cF3 = e3(Im4)
+    sF3 = wct.e3(styleImg)
+    cF3 = wct.e3(Im4)
     sF3 = sF3.data.cpu().squeeze(0)
     cF3 = cF3.data.cpu().squeeze(0)
-    csF3 = feature_wct(cF3,sF3,csF)
-    Im3 = d3(csF3)
+    csF3 = wct.transform(cF3,sF3,csF,args.alpha)
+    Im3 = wct.d3(csF3)
 
-    sF2= e2(styleImg)
-    cF2 = e2(Im3)
+    sF2 = wct.e2(styleImg)
+    cF2 = wct.e2(Im3)
     sF2 = sF2.data.cpu().squeeze(0)
     cF2 = cF2.data.cpu().squeeze(0)
-    csF2 = feature_wct(cF2,sF2,csF)
-    Im2 = d2(csF2)
+    csF2 = wct.transform(cF2,sF2,csF,args.alpha)
+    Im2 = wct.d2(csF2)
 
-    sF1 = e1(styleImg)
-    cF1 = e1(Im2)
+    sF1 = wct.e1(styleImg)
+    cF1 = wct.e1(Im2)
     sF1 = sF1.data.cpu().squeeze(0)
     cF1 = cF1.data.cpu().squeeze(0)
-    csF1 = feature_wct(cF1,sF1,csF)
-    Im1 = d1(csF1)
+    csF1 = wct.transform(cF1,sF1,csF,args.alpha)
+    Im1 = wct.d1(csF1)
     # save_image has this wired design to pad images with 4 pixels at default.
     vutils.save_image(Im1.data.cpu().float(),os.path.join(args.outf,imname))
     return
@@ -149,8 +89,6 @@ avgTime = 0
 cImg = torch.Tensor()
 sImg = torch.Tensor()
 csF = torch.Tensor()
-cImg = Variable(cImg)
-sImg = Variable(sImg)
 csF = Variable(csF)
 if(args.cuda):
     cImg = cImg.cuda()
@@ -159,8 +97,8 @@ if(args.cuda):
 for i,(contentImg,styleImg,imname) in enumerate(loader):
     imname = imname[0]
     print('Transferring ' + imname)
-    cImg.data.resize_(contentImg.size()).copy_(contentImg)
-    sImg.data.resize_(styleImg.size()).copy_(styleImg)
+    cImg = Variable(contentImg.cuda(0),volatile=True)
+    sImg = Variable(styleImg.cuda(0),volatile=True)
     start_time = time.time()
     # WCT Style Transfer
     styleTransfer(cImg,sImg,imname,csF)
--- util.py
+++ util.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import torch
 from torch.utils.serialization import load_lua
+import torchvision.transforms as transforms
 import numpy as np
 import argparse
 import time
@@ -8,49 +9,88 @@ import os
 from PIL import Image
 from modelsNIPS import decoder1,decoder2,decoder3,decoder4,decoder5
 from modelsNIPS import encoder1,encoder2,encoder3,encoder4,encoder5
+import torch.nn as nn
 
 
-def loadModel(args):
 
-    # load pre-trained network
-    vgg1 = load_lua(args.vgg1)
-    decoder1_torch = load_lua(args.decoder1)
-    vgg2 = load_lua(args.vgg2)
-    decoder2_torch = load_lua(args.decoder2)
-    vgg3 = load_lua(args.vgg3)
-    decoder3_torch = load_lua(args.decoder3)
-    vgg4 = load_lua(args.vgg4)
-    decoder4_torch = load_lua(args.decoder4)
-    vgg5 = load_lua(args.vgg5)
-    decoder5_torch = load_lua(args.decoder5)
+class WCT(nn.Module):
+    def __init__(self,args):
+        super(WCT, self).__init__()
+        # load pre-trained network
+        vgg1 = load_lua(args.vgg1)
+        decoder1_torch = load_lua(args.decoder1)
+        vgg2 = load_lua(args.vgg2)
+        decoder2_torch = load_lua(args.decoder2)
+        vgg3 = load_lua(args.vgg3)
+        decoder3_torch = load_lua(args.decoder3)
+        vgg4 = load_lua(args.vgg4)
+        decoder4_torch = load_lua(args.decoder4)
+        vgg5 = load_lua(args.vgg5)
+        decoder5_torch = load_lua(args.decoder5)
 
 
-    e1 = encoder1(vgg1)
-    d1 = decoder1(decoder1_torch)
-    e2 = encoder2(vgg2)
-    d2 = decoder2(decoder2_torch)
-    e3 = encoder3(vgg3)
-    d3 = decoder3(decoder3_torch)
-    e4 = encoder4(vgg4)
-    d4 = decoder4(decoder4_torch)
-    e5 = encoder5(vgg5)
-    d5 = decoder5(decoder5_torch)
-    if(args.cuda):
-        e1.cuda()
-        e2.cuda()
-        e3.cuda()
-        e4.cuda()
-        e5.cuda()
-        d1.cuda()
-        d2.cuda()
-        d3.cuda()
-        d4.cuda()
-        d5.cuda()
+        self.e1 = encoder1(vgg1)
+        self.d1 = decoder1(decoder1_torch)
+        self.e2 = encoder2(vgg2)
+        self.d2 = decoder2(decoder2_torch)
+        self.e3 = encoder3(vgg3)
+        self.d3 = decoder3(decoder3_torch)
+        self.e4 = encoder4(vgg4)
+        self.d4 = decoder4(decoder4_torch)
+        self.e5 = encoder5(vgg5)
+        self.d5 = decoder5(decoder5_torch)
 
-    # save some space
-    del vgg1; del decoder1_torch
-    del vgg2; del decoder2_torch
-    del vgg3; del decoder3_torch
-    del vgg4; del decoder4_torch
-    del vgg5; del decoder5_torch
-    return e1,d1,e2,d2,e3,d3,e4,d4,e5,d5
+    def whiten_and_color(self,cF,sF):
+        start_time = time.time()
+        cFSize = cF.size()
+        c_mean = torch.mean(cF,1) # c x (h x w)
+        c_mean = c_mean.unsqueeze(1).expand_as(cF)
+        cF = cF - c_mean
+
+        contentConv = torch.mm(cF,cF.t()).div(cFSize[1]-1) + torch.eye(cFSize[0]).double()
+        c_u,c_e,c_v = torch.svd(contentConv,some=False)
+
+        k_c = cFSize[0]
+        for i in range(cFSize[0]):
+            if c_e[i] < 0.00001:
+                k_c = i
+                break
+
+        sFSize = sF.size()
+        s_mean = torch.mean(sF,1)
+        sF = sF - s_mean.unsqueeze(1).expand_as(sF)
+        styleConv = torch.mm(sF,sF.t()).div(sFSize[1]-1)
+        s_u,s_e,s_v = torch.svd(styleConv,some=False)
+
+        k_s = sFSize[0]
+        for i in range(sFSize[0]):
+            if s_e[i] < 0.00001:
+                k_s = i
+                break
+
+        c_d = (c_e[0:k_c]).pow(-0.5)
+        step1 = torch.mm(c_v[:,0:k_c],torch.diag(c_d))
+        step2 = torch.mm(step1,(c_v[:,0:k_c].t()))
+        whiten_cF = torch.mm(step2,cF)
+
+        s_d = (s_e[0:k_s]).pow(0.5)
+        targetFeature = torch.mm(torch.mm(torch.mm(s_v[:,0:k_s],torch.diag(s_d)),(s_v[:,0:k_s].t())),whiten_cF)
+        targetFeature = targetFeature + s_mean.unsqueeze(1).expand_as(targetFeature)
+        end_time = time.time()
+        print("WCT time is %f."%(end_time - start_time))
+        return targetFeature
+
+    def transform(self,cF,sF,csF,alpha):
+        cF = cF.double()
+        sF = sF.double()
+        C,W,H = cF.size(0),cF.size(1),cF.size(2)
+        _,W1,H1 = sF.size(0),sF.size(1),sF.size(2)
+        cFView = cF.view(C,-1)
+        sFView = sF.view(C,-1)
+
+        targetFeature = self.whiten_and_color(cFView,sFView)
+        targetFeature = targetFeature.view_as(cF)
+        ccsF = alpha * targetFeature + (1.0 - alpha) * cF
+        ccsF = ccsF.float().unsqueeze(0)
+        csF.data.resize_(ccsF.size()).copy_(ccsF)
+        return csF
